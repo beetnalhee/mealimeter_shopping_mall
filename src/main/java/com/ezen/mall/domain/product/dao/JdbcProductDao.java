daa9f27ce2750794915ce09d0929858c10a3defc
chan999 <cksrb8702@gmail.com>
2024-04-12 14:30:41 +0900

    리뷰 HTML 및 CSS 추가
    리뷰 페이지 HTML 및 CSS 생성하였습니다.

--- src/main/java/com/ezen/mall/domain/product/dao/JdbcProductDao.java
+++ src/main/java/com/ezen/mall/domain/product/dao/JdbcProductDao.java
@@ -14,7 +14,6 @@ import java.util.List;
 
 public class JdbcProductDao implements ProductDao {
     private ConnectionFactory connectionFactory;
-
     Connection conn = null;
     PreparedStatement pstmt = null;
     ResultSet rs = null;
